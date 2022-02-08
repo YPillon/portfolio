64ad320603176703f01f1256ea537986d4cb13d8
Younes <younespillon@gmail.com>
2022-02-08 19:23:22 +0100

    Animations terminées, menu mobile en place.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+//Displays the current year in the footer
 document.getElementById("currentYear").innerText = new Date().getFullYear();
+
+//Hamburger menu- on click of link, minimise overlay nav
+for (link of document.querySelectorAll("#navLinks li")) {
+  link.onclick = function () {
+    document.querySelector("input[type=checkbox]").checked = false;
+  };
+}
